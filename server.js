238486397a893d32935b22b778e0173cb887fbe3
Faris Halteh <Faris.halteh@gmail.com>
2024-05-09 15:11:06 +0200

    Add TaskRoutes

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const taskRoutes = require('./taskRoutes.js');
 
 app.use(express.json());
 
+app.use('/api', taskRoutes)
 app.get('/', (req, res) => {
     res.send("Hello world")
 });
-
 app.listen(PORT, () => {
     console.log("Hello, the server is up and running now!")
 })
